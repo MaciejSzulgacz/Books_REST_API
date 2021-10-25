bb7f3084a6ba7668e3ffa4d08be10e5e03fc11b4
Maciej <maciej.szulgacz@gmail.com>
2021-10-25 21:57:30 +0200

    frame of post method

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
     return "<h1>Hello World!</h1>"
+
+
+@app.route("/db", method=['POST'])
+def update_data_set():
+    if request.method == 'POST':
+        return 'Good request', 200
+    else:
+        return 'Bad request', 400
+
+
